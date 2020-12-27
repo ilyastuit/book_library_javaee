4a93ad1067036b74d9043a608d8544223d1d2e60
ilyastuit <ilyastuit@gmail.com>
2020-12-27 09:04:08 +0300

    Added Author List functionality

---
+++ src/main/java/ru/bookshop/training/web/beans/Author.java
@@ -0,0 +1,23 @@
+package ru.bookshop.training.web.beans;
+
+public class Author {
+
+    public Author() {
+    }
+
+    private String name;
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
